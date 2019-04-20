39bf468caf60df9eb7aa7d87af8beca6b5eea78c
Kristina <79803775144@yandex.ru>
2019-04-21 07:53:01 +1100

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict'
+
+//console.log(leftBorderWidth);
+
+var leftBorderWidth = 1;
+let second = 2; 
+const pi = 3.14;
+
+//console.log(second);
+//console.log('string'/9);
+ let persone = {
+     name: "John", 
+     age: 25,
+     isMarried: false
+ };
+
+
+ console.log(persone["name"]);
+
+
+ let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+ console.log(arr[2]);
+
+ //alert("Hello Word!");
+
+// let answer = confirm("polsovatel?");
+ //console.log(answer);
+
+ //let answer = prompt("Est vosemnadsat?" , "Da");
+ //console.log(typeof(arr));
+
+//console.log("arr" + "- object");
+
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log("2" === 2);  
+let isChecket = true,
+    isClose = true;
+
+console.log(isChecket && isClose);
